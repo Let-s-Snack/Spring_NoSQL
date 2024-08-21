1f7e0f00fe1247887104b9baf6e18a8a9b9ead06
Gustavo-Teotonio <gustavohidroar07@gmail.com>
2024-08-20 22:51:53 -0300

    Fazendo a inserção das dependencias necessárias, junto da conexão com o mongo e a criação do padrão MVC

---
+++ Spring_NoSQL/src/test/java/org/example/spring_nosql/SpringNoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNoSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
